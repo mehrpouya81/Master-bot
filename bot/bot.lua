c3c17756d218aef93234902121a9b6050f0bb32e
mehrpouya81 <mehrpouya81@gmail.com>
2015-12-07 09:17:35 -0800

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -227,7 +227,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {114429017,91054649},
+    sudo_users = {114429017,91054649,175675567},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
